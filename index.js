3aa1d457e1ec75fefde15b6566885c20f99c247e
hmbadhon2 <hmmizanurrahmanbadhon@gmail.com>
2023-01-30 15:34:50 +0600

    cors problem solving

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express();
-require('dotenv').config();
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const port = process.env.port || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require ('cors')                                             
+const jwt = require('jsonwebtoken')
+const app= express()
+require('dotenv').config()
+const port = process.env.PORT||5000
 
 // middleWare
 
@@ -11,7 +12,7 @@ app.use(cors());
 app.use(express.json())
 
 // mongoDB connection
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hvwcwlz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri)
